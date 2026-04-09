0e77439e0a7112eba62ce3a269c912e552105a9d
k4lizen <124312252+k4lizen@users.noreply.github.com>
2026-04-10 00:55:36 +0200

    proper json output

--- r10k/cpu.py
+++ r10k/cpu.py
@@ -4,14 +4,30 @@ from pathlib import Path
 from typing import Any
 
 
+def str_bool(b: bool) -> str:
+    return "true" if b else "false"
+
+
 class PhysicalRegisterFile:
     def __init__(self) -> None:
         self.regs: list[int] = [0] * 64
+
+    def __str__(self) -> str:
+        res: str = '    "PhysicalRegisterFile": [\n      '
+        res += ", ".join([str(x) for x in self.regs])
+        res += "\n    ],\n"
+        return res
 
 
 class DecodedInstructionRegister:
     def __init__(self) -> None:
         self.decoded_pcs: list[int] = [0] * 4
+
+    def __str__(self) -> str:
+        res: str = '    "DecodedInstructionRegister": [\n      '
+        res += ", ".join([str(x) for x in self.decoded_pcs])
+        res += "\n    ],\n"
+        return res
 
 
 class RegisterMapTable:
@@ -19,6 +35,12 @@ class RegisterMapTable:
         # self.logical_to_physical[4] means
         # "Which physical register is mapped to the logical register x4?"
         self.logical_to_physical: list[int] = list(range(32))
+
+    def __str__(self) -> str:
+        res: str = '    "RegisterMapTable": [\n      '
+        res += ", ".join([str(x) for x in self.logical_to_physical])
+        res += "\n    ],\n"
+        return res
 
 
 class FreeList:
@@ -28,10 +50,21 @@ class FreeList:
         self.tail: int = 32
         self.freelist: list[int] = list(range(32))
 
+    def __str__(self) -> str:
+        res: str = '    "FreeList": [\n      '
+        res += ", ".join([str(x) for x in self.freelist])
+        res += "\n    ],\n"
+        return res
 
 class BusyBitTable:
     def __init__(self) -> None:
         self.is_busy: list[bool] = [False] * 64
+
+    def __str__(self) -> str:
+        res: str = '    "BusyBitTable": [\n      '
+        res += ", ".join([str_bool(x) for x in self.is_busy])
+        res += "\n    ],\n"
+        return res
 
 
 @dataclass
@@ -42,15 +75,33 @@ class ActiveListEntry:
     old_destination: int
     pc: int
 
+    def __str__(self) -> str:
+        return f"""\
+      {{
+        "Done": {str_bool(self.done)},
+        "Exception": {str_bool(self.exception)},
+        "LogicalDestination": {self.logical_destination},
+        "OldDestination": {self.old_destination},
+        "PC": {self.pc},
+      }},
+"""
+
 
 # The Reorder Buffer (ROB).
 class ActiveList:
     def __init__(self) -> None:
         self.head: int = 0
-        self.tail: int = 1
+        self.tail: int = 0
         self.the_list: list[ActiveListEntry] = [
             ActiveListEntry(False, False, 0, 0, 0) for _ in range(32)
         ]
+
+    def __str__(self) -> str:
+        res: str = '    "ActiveList": [\n'
+        for el in self.the_list[self.head : self.tail]:
+            res += str(el)
+        res += "    ],\n"
+        return res
 
 
 @dataclass
@@ -65,15 +116,39 @@ class IntegerQueueEntry:
     opcode: str
     pc: int
 
+    def __str__(self) -> str:
+        return f"""\
+      {{
+        "DestRegister": {self.dest_register},
+        "OpAIsReady": {str_bool(self.opa_is_ready)},
+        "OpARegTag": {self.opa_reg_tag},
+        "OpAValue": {self.opa_value},
+        "OpBIsReady": {str_bool(self.opb_is_ready)},
+        "OpBRegTag": {self.opb_reg_tag},
+        "OpBValue": {self.opb_value},
+        "OpCode": "{self.opcode}",
+        "PC": {self.pc}
+      }},
+"""
+
 
 # The reservation station.
 class IntegerQueue:
     def __init__(self) -> None:
         self.head: int = 0
-        self.tail: int = 1
+        self.tail: int = 0
         self.queue: list[IntegerQueueEntry] = [
             IntegerQueueEntry(0, False, 0, 0, False, 0, 0, "", 0) for _ in range(32)
         ]
+
+    def __str__(self) -> str:
+        res: str = '    "IntegerQueue": [\n'
+        for el in self.queue[self.head : self.tail]:
+            res += str(el)
+        # no comma since it's the last structure
+        res += "    ]\n"
+
+        return res
 
 
 class CPU:
@@ -81,7 +156,7 @@ class CPU:
         self.input_instructions: list[str] = input_instructions
         self.out_file_path: Path = out_file_path
 
-        self.state_log: list[dict[Any, Any]] = []
+        self.state_log: str = "[\n"
 
         self.pc: int = 0
         self.exception_pc: int = 0
@@ -96,7 +171,18 @@ class CPU:
         self.integer_queue = IntegerQueue()
 
     def dump_state_into_log(self) -> None:
-        print("dump inump")
+        self.state_log += "  {\n"
+        self.state_log += f'    "PC": {self.pc},\n'
+        self.state_log += str(self.reg_file)
+        self.state_log += str(self.decoded_instr_reg)
+        self.state_log += f'    "ExceptionPC": {self.exception_pc},\n'
+        self.state_log += f'    "Exception": {str_bool(self.exception)},\n'
+        self.state_log += str(self.reg_map_table)
+        self.state_log += str(self.freelist)
+        self.state_log += str(self.busy_bit_table)
+        self.state_log += str(self.active_list)
+        self.state_log += str(self.integer_queue)
+        self.state_log += "  },\n"
 
     def is_active_empty(self) -> bool:
         return True
@@ -108,9 +194,9 @@ class CPU:
         pass
 
     def save_log(self) -> None:
-        out_contents = json.dumps(self.state_log)
+        self.state_log += "]"
         with self.out_file_path.open("w") as outfile:
-            outfile.write(out_contents)
+            outfile.write(self.state_log)
 
         print(f"Saved to {self.out_file_path}!")
 
@@ -119,6 +205,9 @@ class CPU:
 
     def run_completely(self) -> None:
         self.dump_state_into_log()
+        self.save_log()
+
+        return
 
         while not (self.no_instr_left() and self.is_active_empty()):
             self.propagate()
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 r10k/entry.py $1 $2
+r10k $1 $2
